a900d43d38687bb9776a595ed1ffaf3a7bd6996e
alammehtab <mehtaba728@gmail.com>
2023-01-27 16:39:38 +0500

    user crud is ready without exception handling

--- spring-boot/blog-app-backend/src/main/java/com/alam/blogappbackend/controllers/UserController.java
+++ spring-boot/blog-app-backend/src/main/java/com/alam/blogappbackend/controllers/UserController.java
@@ -1,11 +1,14 @@
 package com.alam.blogappbackend.controllers;
 
+import com.alam.blogappbackend.dtos.ApiResponse;
 import com.alam.blogappbackend.dtos.UserDto;
 import com.alam.blogappbackend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -14,10 +17,39 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    // create user
     @PostMapping("/new")
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
         UserDto createdUserDto = userService.createUser(userDto);
 
         return new ResponseEntity<>(createdUserDto, HttpStatus.CREATED);
+    }
+
+    // update user
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable Integer userId){
+        UserDto updatedUser = this.userService.updateUser(userDto,userId);
+
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    // delete user
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Integer userId){
+         this.userService.deleteUserById(userId);
+
+         return new ResponseEntity(new ApiResponse("User deleted successfully.",true),HttpStatus.OK);
+    }
+
+    // get all users
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> getAllUsers(){
+        return ResponseEntity.ok(this.userService.getAllUsers());
+    }
+
+    // get single users
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getSingleUser(@PathVariable Integer userId){
+        return ResponseEntity.ok(this.userService.getUserById(userId));
     }
 }
--- spring-boot/blog-app-backend/src/test/java/com/alam/blogappbackend/BlogAppBackendApplicationTests.java
+++ spring-boot/blog-app-backend/src/test/java/com/alam/blogappbackend/BlogAppBackendApplicationTests.java
@@ -1,11 +1,14 @@
 package com.alam.blogappbackend;
 
+import com.alam.blogappbackend.repositories.UserRepo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlogAppBackendApplicationTests {
-
+	@Autowired
+	private UserRepo userRepo;
 	@Test
 	void contextLoads() {
 	}
