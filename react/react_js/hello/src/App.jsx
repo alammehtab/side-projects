1b8aa4668a039fd5e4006fa8040683afa848888e
alammehtab <mehtaba728@gmail.com>
2023-02-09 16:43:32 +0500

    added required libraries, logo and basic configurations

---
+++ react/react_js/hello/src/App.jsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import "./App.css";
+
+function App() {
+  return (
+    <Box className="App">
+      <Typography>Helllllooooooooo</Typography>
+    </Box>
+  );
+}
+
+export default App;
