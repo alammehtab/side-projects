42a8dd7b233348f5f01c483aae814de9db9b9ff8
alammehtab <mehtaba728@gmail.com>
2023-02-17 12:18:14 +0500

    added id to postDto

--- spring-boot/blog-app-backend/src/main/java/com/alam/blogappbackend/dtos/PostDto.java
+++ spring-boot/blog-app-backend/src/main/java/com/alam/blogappbackend/dtos/PostDto.java
@@ -12,6 +12,7 @@ import java.util.Date;
 @Setter
 @NoArgsConstructor
 public class PostDto {
+    private Integer Id;
     private String title;
     private String content;
     private String imageName;
