fae109bd8efa87bd68998e19fe12874757a89eee
alammehtab <mehtaba728@gmail.com>
2023-02-28 16:48:25 +0500

    added securityConfig and converted formBasedAuth to basicAuth using without adapter springboot 3 technique

---
+++ spring-boot/blog-app-backend/src/main/java/com/alam/blogappbackend/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.alam.blogappbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig{
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
