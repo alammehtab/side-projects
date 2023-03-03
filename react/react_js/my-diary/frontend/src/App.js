bb2d5bede5c51a400b67474ab172b302bc0a02c5
alammehtab <mehtaba728@gmail.com>
2023-03-03 15:57:45 +0500

    added Header component

--- react/react_js/my-diary/frontend/src/App.js
+++ react/react_js/my-diary/frontend/src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <Router>
         <div className="container">
+          <Header />
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/register" element={<Register />} />
