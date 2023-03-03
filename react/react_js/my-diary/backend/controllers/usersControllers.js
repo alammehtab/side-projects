ad621d276ec1a3692f366a4d4d8964dfdbe41108
alammehtab <mehtaba728@gmail.com>
2023-03-03 10:26:08 +0500

    added registerUser with encrypted password

--- react/react_js/my-diary/backend/controllers/usersControllers.js
+++ react/react_js/my-diary/backend/controllers/usersControllers.js
@@ -1,12 +1,46 @@
 // could use try-catch to handle the async errors
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 // @desc    Register user
 // @route   POST /api/v1/users/register
 // @access  public
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Register user" });
+  const { name, email, password } = req?.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
+  // check if user exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // create user and throw to db
+  const user = await User.create({ name, email, password: hashedPassword });
+
+  // if user is created
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Sorry, user not created");
+  }
 });
 
 // @desc    Login a user
