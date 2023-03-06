16a55b08b891b0edcd2227d6f4d246e8f4d8be75
alammehtab <mehtaba728@gmail.com>
2023-03-06 16:37:47 +0500

    protected the dashboard

--- react/react_js/my-diary/frontend/src/pages/Dashboard.jsx
+++ react/react_js/my-diary/frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [navigate, user]);
+
   return <div>Dashboard</div>;
 };
 
