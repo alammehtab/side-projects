5338a585bd97cca2ddd4f2ea264d4f63acd3bdf4
alammehtab <mehtaba728@gmail.com>
2023-03-10 15:12:12 +0500

    added register and login with encrypted password and jwt

--- react/react_js/hello/backend/controllers/userControllers.js
+++ react/react_js/hello/backend/controllers/userControllers.js
@@ -1,9 +1,10 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 //@description     Register new user
-//@route           POST /api/user/
+//@route           POST /api/v1/users/register
 //@access          Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, pic } = req.body;
@@ -20,10 +21,14 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("User already exists");
   }
 
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
   const user = await User.create({
     name,
     email,
-    password,
+    password: hashedPassword,
     pic,
   });
 
@@ -42,9 +47,37 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+//@description     Login user
+//@route           POST /api/v1/users/login
+//@access          Public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please Enter all the Feilds");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateJwt(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
+});
+
 // generate JWT
 const generateJwt = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
 
-module.exports = { registerUser };
+module.exports = { registerUser, loginUser };
--- react/react_js/hello/backend/routes/userRoutes.js
+++ react/react_js/hello/backend/routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { registerUser } = require("../controllers/userControllers");
+const { registerUser, loginUser } = require("../controllers/userControllers");
 const router = express.Router();
 
 router.post("/register", registerUser);
-router.post("/login", (req, res) => res.json({ message: "login user" }));
+router.post("/login", loginUser);
 
 module.exports = router;
