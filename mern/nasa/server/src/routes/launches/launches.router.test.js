67b8b3b3f3bf13ed185466d0785eb7e5a8b8c57d
alammehtab <mehtaba728@gmail.com>
2023-05-29 14:54:22 +0500

    load planets data in test

--- mern/nasa/server/src/routes/launches/launches.router.test.js
+++ mern/nasa/server/src/routes/launches/launches.router.test.js
@@ -1,10 +1,12 @@
 const request = require("supertest");
 const app = require("../../app");
+const { loadPlanetsData } = require("../../models/planets/planets.model");
 const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
 
 describe("Launches API", () => {
   beforeAll(async () => {
     await mongoConnect();
+    await loadPlanetsData();
   });
 
   afterAll(async () => {
