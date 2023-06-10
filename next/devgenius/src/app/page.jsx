fb042d2e0fe4d5e3be59ce52d6fa848fba437b02
alammehtab <mehtaba728@gmail.com>
2023-06-10 18:32:33 +0500

    complete home page

--- next/devgenius/src/app/page.jsx
+++ next/devgenius/src/app/page.jsx
@@ -5,10 +5,19 @@ import HeroImage from "public/hero.png";
 export default function Home() {
   return (
     <div className={styles.container}>
-      {/* if importing images as components, you don't have to specify width and height,
+      <div className={styles.item}>
+        <h1 className={styles.title}>Unlocking Limitless Possibilities</h1>
+        <p className={styles.description}>
+          We turn your ideas into reality with the magic of our awesome team.
+        </p>
+        <button className={styles.seeOurWorkBtn}>See Our Work</button>
+      </div>
+      <div className={styles.item}>
+        {/* if importing images as components, you don't have to specify width and height,
         otherwise you have to
       */}
-      <Image src={HeroImage} alt="hero-image" className={styles.heroImg} />
+        <Image src={HeroImage} alt="hero-image" className={styles.heroImg} />
+      </div>
     </div>
   );
 }
