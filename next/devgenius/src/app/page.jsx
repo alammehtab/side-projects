d2378388877e7884cbee470b91f14266231f30ca
alammehtab <mehtaba728@gmail.com>
2023-06-10 17:36:50 +0500

    add heroImg to main page using direct import method

--- next/devgenius/src/app/page.jsx
+++ next/devgenius/src/app/page.jsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import HeroImage from "public/hero.png";
 
 export default function Home() {
-  return <div className={styles.container}>Home</div>;
+  return (
+    <div className={styles.container}>
+      {/* if importing images as components, you don't have to specify width and height,
+        otherwise you have to
+      */}
+      <Image src={HeroImage} alt="hero-image" className={styles.heroImg} />
+    </div>
+  );
 }
