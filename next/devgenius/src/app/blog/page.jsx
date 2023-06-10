75ca0a0d4225b6ac0ad2370ea555e1a1f4f2c438
alammehtab <mehtaba728@gmail.com>
2023-06-10 14:54:32 +0500

    add images to public, setup pages routes

---
+++ next/devgenius/src/app/blog/page.jsx
@@ -0,0 +1,5 @@
+const Blog = () => {
+  return <div>Blog</div>;
+};
+
+export default Blog;
---
+++ next/devgenius/src/app/portfolio/page.jsx
@@ -0,0 +1,5 @@
+const Portfolio = () => {
+  return <div>Portfolio</div>;
+};
+
+export default Portfolio;
