617929cdb4f9e91310ce2bff1ac7c704b0d06611
alammehtab <mehtaba728@gmail.com>
2023-06-11 10:27:07 +0500

    setup basic structure of main blogs page

--- next/devgenius/src/app/blog/page.jsx
+++ next/devgenius/src/app/blog/page.jsx
@@ -1,7 +1,57 @@
+import Link from "next/link";
 import styles from "./blog.module.css";
+import Image from "next/image";
 
 const Blog = () => {
-  return <div className={styles.container}>Blog</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/illustration.png"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>Description</p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/illustration.png"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>Description</p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/illustration.png"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>Description</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Blog;
