954257d8d14a81d4f99829c29d65ac3782920d09
alammehtab <mehtaba728@gmail.com>
2023-06-11 10:08:53 +0500

    complete blog category page

--- next/devgenius/src/app/portfolio/[category]/page.jsx
+++ next/devgenius/src/app/portfolio/[category]/page.jsx
@@ -1,6 +1,94 @@
+import Button from "@/app/components/button/Button";
 import styles from "./portCat.module.css";
-const PortfolioCategory = () => {
-  return <div className={styles.container}>PortfolioCategory</div>;
+import Image from "next/image";
+const PortfolioCategory = ({ params }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.categoryTitle}>{params.category}</h1>
+
+      {/* first item */}
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.itemTitle}>Lorem ipsum dolor sit amet</h1>
+          <p className={styles.categoryDesc}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+            officia neque sint aliquid odit harum necessitatibus nulla ut eius
+            maiores.
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+            numquam dolores corporis accusamus voluptatibus aspernatur molestias
+            libero exercitationem voluptatum aliquam. Atque saepe temporibus
+            corporis libero hic pariatur quisquam vitae et!
+          </p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="/apps.jpg"
+            alt="appsCategory-image"
+          />
+        </div>
+      </div>
+
+      {/* second item */}
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.itemTitle}>Lorem ipsum dolor sit amet</h1>
+          <p className={styles.categoryDesc}>
+            <p className={styles.categoryDesc}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Recusandae officia neque sint aliquid odit harum necessitatibus
+              nulla ut eius maiores.
+              <br />
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+              numquam dolores corporis accusamus voluptatibus aspernatur
+              molestias libero exercitationem voluptatum aliquam. Atque saepe
+              temporibus corporis libero hic pariatur quisquam vitae et!
+            </p>
+          </p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="/apps.jpg"
+            alt="appsCategory-image"
+          />
+        </div>
+      </div>
+
+      {/* third item */}
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.itemTitle}>Lorem ipsum dolor sit amet</h1>
+          <p className={styles.categoryDesc}>
+            <p className={styles.categoryDesc}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Recusandae officia neque sint aliquid odit harum necessitatibus
+              nulla ut eius maiores.
+              <br />
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+              numquam dolores corporis accusamus voluptatibus aspernatur
+              molestias libero exercitationem voluptatum aliquam. Atque saepe
+              temporibus corporis libero hic pariatur quisquam vitae et!
+            </p>
+          </p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="/apps.jpg"
+            alt="appsCategory-image"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PortfolioCategory;
