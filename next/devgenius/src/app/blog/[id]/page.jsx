89ff1881cd217039f0385cf927166d1537563930
alammehtab <mehtaba728@gmail.com>
2023-06-11 10:38:06 +0500

    setup individual blogPost page structure

--- next/devgenius/src/app/blog/[id]/page.jsx
+++ next/devgenius/src/app/blog/[id]/page.jsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
 import styles from "./blogPost.module.css";
 
 const BlogPost = () => {
-  return <div className={styles.container}>BlogPost</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}></p>
+          <div className={styles.author}>
+            <Image
+              src="/illustration.png"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Username</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src="/apps.jpg" alt="" fill={true} className={styles.image} />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>content</p>
+      </div>
+    </div>
+  );
 };
 
 export default BlogPost;
