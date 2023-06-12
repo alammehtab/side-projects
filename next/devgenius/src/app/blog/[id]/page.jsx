59d2ed2db3a477919f1040164a1ff25f48c1808a
alammehtab <mehtaba728@gmail.com>
2023-06-12 14:40:01 +0500

    get blogs page data and blogPost page data on server side

--- next/devgenius/src/app/blog/[id]/page.jsx
+++ next/devgenius/src/app/blog/[id]/page.jsx
@@ -1,13 +1,48 @@
 import Image from "next/image";
 import styles from "./blogPost.module.css";
 
-const BlogPost = () => {
+async function getBlogPostData(postId) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${postId}`,
+    {
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+async function getUserData(userId) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${userId}`,
+    {
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const BlogPost = async ({ params }) => {
+  const blogPost = await getBlogPostData(params.id);
+  const user = await getUserData(blogPost.userId);
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>Title</h1>
-          <p className={styles.desc}></p>
+          <h1 className={styles.title}>{blogPost.title}</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
           <div className={styles.author}>
             <Image
               src="/illustration.png"
@@ -16,7 +51,7 @@ const BlogPost = () => {
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>Username</span>
+            <span className={styles.username}>{user.name}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
@@ -24,7 +59,7 @@ const BlogPost = () => {
         </div>
       </div>
       <div className={styles.content}>
-        <p className={styles.text}>content</p>
+        <p className={styles.text}>{blogPost.body}</p>
       </div>
     </div>
   );
