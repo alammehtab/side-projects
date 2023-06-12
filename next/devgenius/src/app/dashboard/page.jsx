e9b7d36d5c437ec4c8160ce25eab9c579f3ca098
alammehtab <mehtaba728@gmail.com>
2023-06-12 14:56:56 +0500

    get data on dashboard page using client-side tech using useEffect

--- next/devgenius/src/app/dashboard/page.jsx
+++ next/devgenius/src/app/dashboard/page.jsx
@@ -1,6 +1,33 @@
+"use client";
+import { useEffect, useState } from "react";
 import styles from "./dashboard.module.css";
 
 const Dashboard = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        cache: "no-store",
+      });
+      const data = await res.json();
+
+      if (!res.ok) {
+        setError(true);
+      }
+
+      setData(data);
+      setIsLoading(false);
+    };
+
+    getData();
+  }, []);
+
+  console.log(data);
+
   return <div className={styles.container}>Dashboard</div>;
 };
 
