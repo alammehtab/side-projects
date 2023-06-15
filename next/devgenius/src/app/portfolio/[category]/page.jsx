b016e06eb1761bd705a3e14f081bae200b2ff79a
alammehtab <mehtaba728@gmail.com>
2023-06-15 14:21:08 +0500

    fetch data from local json file on blogPost page

--- next/devgenius/src/app/portfolio/[category]/page.jsx
+++ next/devgenius/src/app/portfolio/[category]/page.jsx
@@ -1,92 +1,44 @@
 import Button from "@/app/components/button/Button";
 import styles from "./portCat.module.css";
 import Image from "next/image";
+import { items } from "./data";
+import { notFound } from "next/navigation";
+
+const getGalleryItems = (category) => {
+  const data = items[category];
+
+  if (data) {
+    return data;
+  }
+
+  return notFound();
+};
 const PortfolioCategory = ({ params }) => {
+  const galleryItems = getGalleryItems(params.category);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.categoryTitle}>{params.category}</h1>
 
-      {/* first item */}
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.itemTitle}>Lorem ipsum dolor sit amet</h1>
-          <p className={styles.categoryDesc}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
-            officia neque sint aliquid odit harum necessitatibus nulla ut eius
-            maiores.
-            <br />
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-            numquam dolores corporis accusamus voluptatibus aspernatur molestias
-            libero exercitationem voluptatum aliquam. Atque saepe temporibus
-            corporis libero hic pariatur quisquam vitae et!
-          </p>
-          <Button text="See More" url="#" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            fill={true}
-            src="/apps.jpg"
-            alt="appsCategory-image"
-          />
-        </div>
-      </div>
-
-      {/* second item */}
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.itemTitle}>Lorem ipsum dolor sit amet</h1>
-          <p className={styles.categoryDesc}>
-            <p className={styles.categoryDesc}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Recusandae officia neque sint aliquid odit harum necessitatibus
-              nulla ut eius maiores.
-              <br />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-              numquam dolores corporis accusamus voluptatibus aspernatur
-              molestias libero exercitationem voluptatum aliquam. Atque saepe
-              temporibus corporis libero hic pariatur quisquam vitae et!
-            </p>
-          </p>
-          <Button text="See More" url="#" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            fill={true}
-            src="/apps.jpg"
-            alt="appsCategory-image"
-          />
-        </div>
-      </div>
-
-      {/* third item */}
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.itemTitle}>Lorem ipsum dolor sit amet</h1>
-          <p className={styles.categoryDesc}>
-            <p className={styles.categoryDesc}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Recusandae officia neque sint aliquid odit harum necessitatibus
-              nulla ut eius maiores.
-              <br />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-              numquam dolores corporis accusamus voluptatibus aspernatur
-              molestias libero exercitationem voluptatum aliquam. Atque saepe
-              temporibus corporis libero hic pariatur quisquam vitae et!
-            </p>
-          </p>
-          <Button text="See More" url="#" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            fill={true}
-            src="/apps.jpg"
-            alt="appsCategory-image"
-          />
-        </div>
-      </div>
+      {galleryItems.map((item) => {
+        return (
+          <div className={styles.item} key={item.id}>
+            <div className={styles.content}>
+              <h1 className={styles.itemTitle}>{item.title}</h1>
+              <p className={styles.categoryDesc}>{item.desc}</p>
+              <Button text="See More" url="#" />
+            </div>
+            <div className={styles.imgContainer}>
+              <Image
+                className={styles.img}
+                fill={true}
+                src={item.image}
+                alt="appsCategory-image"
+              />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
