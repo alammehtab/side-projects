bf2f4c6950260b74b9773fa83d96519017f648c5
alammehtab <mehtaba728@gmail.com>
2023-06-22 10:59:09 +0500

    minor fix

--- next/devgenius/src/app/(pages)/contact/page.jsx
+++ next/devgenius/src/app/(pages)/contact/page.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./contact.module.css";
-import Button from "../components/button/Button";
+import Button from "../../components/button/Button";
 
 const Contact = () => {
   return (
